9e7df947a1c20b8c916903e23413dd4dbb1cc588
トモ <caokaocaokao@gmail.com>
2025-11-25 01:05:30 +0800

    Init

---
+++ cogs/basic_commands.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+from main import join_voice_channel, leave_voice_channel
+
+
+class BasicCommands(commands.Cog):
+
+    def __init__(self, client=discord.Client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Command Bot 登入身分: {self.client.user}')
+        custom_activity = discord.CustomActivity('喵喵')
+        await self.client.change_presence(
+            status=discord.Status.online, activity=custom_activity
+        )
+        try:
+            synced = await self.client.tree.sync()
+            print(f"Synced {synced} commands")
+        except Exception as e:
+            print("An error occurred while syncing: ", e)
+
+    @commands.hybrid_command(name='kiki', help='kiki會跟你打招呼')
+    async def kiki(self, ctx: commands.Context):
+        await ctx.defer()
+        await ctx.send('喵喵喵')
+
+    @commands.hybrid_command(name='kiki來', help='可以叫kiki進頻道')
+    async def kiki_come(self, ctx: commands.Context):
+        await ctx.defer()
+        if ctx.author.voice is not None:
+            if ctx.voice_client is not None:
+                await leave_voice_channel(self.client, ctx.voice_client.channel)
+            await join_voice_channel(self.client, ctx.author.voice.channel)
+            await ctx.send('喵')
+        else:
+            await ctx.send('**kiki不知道你想叫她去哪**')        
+
+    @commands.hybrid_command(name='kiki滾', help='無情ㄉ趕走kiki')
+    async def kiki_fuck_off(self, ctx: commands.Context):
+        await ctx.defer()
+        if (ctx.voice_client is not None) and (ctx.author.voice is not None) and (ctx.voice_client.channel == ctx.author.voice.channel):
+            await leave_voice_channel(self.client, ctx.voice_client.channel)
+            await ctx.send(':(')
+        else:
+            await ctx.send('||誰在狗叫||')
+
+
+async def setup(command_bot: commands.Bot):
+    await command_bot.add_cog(BasicCommands(command_bot))
---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import asyncio
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#setup dc bot
+dc_intents = discord.Intents.all()
+dc_bot = commands.Bot(command_prefix = '!', intents=dc_intents, application_id=1194325058882126005)
+
+async def join_voice_channel(
+    client: discord.Client, voice_channel: discord.VoiceChannel, cls=None
+) -> discord.VoiceProtocol:
+    if client.voice_clients is not None:
+        for voice_client in client.voice_clients:
+            if voice_client.channel == voice_channel:
+                return voice_client
+    if cls:
+        return await voice_channel.connect(cls=cls)
+    else:
+        return await voice_channel.connect()
+
+async def leave_voice_channel(
+    client: discord.Client, voice_channel: discord.VoiceChannel
+):
+    for voice_client in client.voice_clients:
+        if voice_client.channel == voice_channel:
+            return await voice_client.disconnect()
+
+async def init_load_all_cog():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await dc_bot.load_extension(f'cogs.{filename[:-3]}')
+
+if __name__ == '__main__':
+    asyncio.run(init_load_all_cog())
+    bot_token = os.environ.get("discord_bot_token")
+    dc_bot.run(bot_token)
